6c7b5da16e3e80f31659bef220533807e8e48607
Ayden Johnson <67842316+ADJohnson314@users.noreply.github.com>
2025-03-20 11:51:25 -0400

    Lab 6: Step 2

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -6,19 +6,36 @@
         endAngle: 2 * Math.PI
     });
 
-    let data = [1, 2, 3, 4, 5, 5];
-    let sliceGenerator = d3.pie();
+    let data = [
+        { value: 1, label: "apples" },
+        { value: 2, label: "oranges" },
+        { value: 3, label: "mangos" },
+        { value: 4, label: "pears" },
+        { value: 5, label: "limes" },
+        { value: 5, label: "cherries" }
+    ];
+    let sliceGenerator = d3.pie().value(d => d.value);
     let arcData = sliceGenerator(data);
     let arcs = arcData.map(d => arcGenerator(d));
 
     let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 </script>
-<svg viewBox="-50 -50 100 100">
-	{#each arcs as arc, index}
-        <path d={ arc } fill={ colors(index) } />
-    {/each}
-</svg>
+<div class="container">
+	<svg viewBox="-50 -50 100 100">
+        {#each arcs as arc, index}
+            <path d={ arc } fill={ colors(index) } />
+        {/each}
+    </svg>
+	<ul class="legend">
+        {#each data as d, index}
+            <li style="--color: { colors(index) }">
+                <span class="swatch"></span>
+                {d.label} <em>({d.value})</em>
+            </li>
+        {/each}
+    </ul>
+</div>
 <style>
     svg {
         max-width: 20em;
@@ -26,5 +43,30 @@
 
         /* Do not clip shapes outside the viewBox */
         overflow: visible;
+    }
+    .swatch {
+        display: inline-flex;
+        width: 8px;
+        height: 8px;
+        background-color: var(--color);
+    }
+    .legend {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(8em, 1fr));
+        border: 1px solid black;
+        padding: 4px;
+        margin: 4px;
+        flex: 1;
+        height: auto;
+    }
+    li {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+    }
+    .container {
+        display: flex;
+        gap: 1em;
+        align-items: center;
     }
 </style>
