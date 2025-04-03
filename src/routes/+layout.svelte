a523a847384cadf8c4818dd8b609446ab20e0edf
Ayden Johnson <67842316+ADJohnson314@users.noreply.github.com>
2025-04-03 12:13:52 -0400

    Lab 7: Step 0

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
     { url: "./projects", title: "Projects" },
     { url: "./contact", title: "Contact" },
     { url: "./resume", title: "Resume" },
-    /*{ url: "./meta", title: "Meta" },*/
+    { url: "./meta", title: "Meta" },
     {url: "https://github.com/ADJohnson314", title: "Github"},
     ];
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,28 @@
 <svelte:head>
   <title>Ayden Johnson: Personal site and 6.C35 portfolio</title>
 </svelte:head>
+
 <script>
     import projects from "$lib/projects.json";
     import Project from "$lib/Project.svelte";
+
+    import { onMount } from "svelte";
+
+  let githubData = null;
+  let loading = true;
+  let error = null;
+
+  onMount(async () => {
+    try {
+      const response = await fetch("https://api.github.com/users/ADJohnson314");
+      githubData = await response.json();
+    } catch (err) {
+      error = err;
+    }
+    loading = false;
+  });
 </script>
+
 <h1>Ayden Johnson</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quotidie futurum me imaginari ubi tecum esse possum. In manu mea stylus qui carmen scribet de me et tu.</p>
 <img src="images/Windows Beach Background.jpg" alt="A rock formation on a beach">
@@ -14,28 +32,23 @@
     <Project data={p} hLevel="3" />
     {/each}
 </div>
-{#await fetch("https://api.github.com/users/ADJohnson314")}
-  <p>Loading...</p>
-{:then response}
-  {#await response.json()}
-    <p>Decoding...</p>
-  {:then data}
-    <section>
-      <h2>My GitHub Stats</h2>
-      <dl>
-        <dt>Followers:</dt>
-        <dd>{data.followers}</dd>
-        <dt>Following:</dt>
-        <dd>{data.following}</dd>
-        <dt>Public Repositories:</dt>
-        <dd>{data.public_repos}</dd>
-      </dl>
-    </section>
-  {:catch error}
+{#if loading}
+    <p>Loading...</p>
+{:else if error}
     <p class="error">Something went wrong: {error.message}</p>
-  {/await}
-{:catch error}
-  <p class="error">Something went wrong: {error.message}</p>
-{/await}
+{:else}
+    <section>
+        <h2>My GitHub Stats</h2>
+        <dl>
+            <dt>Followers</dt>
+            <dd>{githubData.followers}</dd>
+            <dt>Following</dt>
+            <dd>{githubData.following}</dd>
+            <dt>Public Repositories</dt>
+            <dd>{githubData.public_repos}</dd>
+        </dl>
+    </section>
+{/if}
+
 
 
