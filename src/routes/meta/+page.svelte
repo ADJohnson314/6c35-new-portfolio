736b79cc46cf29e99e76f3d44b9a6644424787fc
Ayden Johnson <67842316+ADJohnson314@users.noreply.github.com>
2025-04-10 21:16:53 -0400

    Lab 7: Step 2

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -1,6 +1,131 @@
 <svelte:head>
   <title>Meta - Ayden Johnson: Personal site and 6.C35 portfolio</title>
 </svelte:head>
+<script>
+  import * as d3 from "d3";
+  import { onMount } from "svelte";
+
+  let width = 1000, height = 600;
+  let data = [];
+  let commits = [];
+  let margin = {top: 10, right: 10, bottom: 30, left: 20};
+
+  let usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left
+  };
+  usableArea.width = usableArea.right - usableArea.left;
+  usableArea.height = usableArea.bottom - usableArea.top;
+
+  let xAxis, yAxis;
+  $: {
+    d3.select(xAxis).call(d3.axisBottom(xScale));
+    d3.select(yAxis).call(d3.axisLeft(yScale));
+    d3.select(yAxis).call(d3.axisLeft(yScale).tickFormat(d => String(d % 24).padStart(2, "0") + ":00"));
+  }
+
+  let yAxisGridlines;
+  $: {
+    d3.select(yAxisGridlines).call(
+      d3.axisLeft(yScale)
+        .tickFormat("")
+        .tickSize(-usableArea.width)
+    );
+  }
+
+
+
+  $: minDate = d3.min(commits.map(d => d.date));
+  $: maxDate = d3.max(commits.map(d => d.date));
+  $: maxDatePlusOne = new Date(maxDate);
+  $: maxDatePlusOne.setDate(maxDatePlusOne.getDate() + 1);
+
+  $: xScale = d3.scaleTime()
+                .domain([minDate, maxDatePlusOne])
+                .range([usableArea.left, usableArea.right])
+                .nice();
+
+  $: yScale = d3.scaleLinear()
+                .domain([24, 0])
+                .range([usableArea.bottom, usableArea.top]);
+
+
+  onMount(async () => {
+    data = await d3.csv("/loc.csv", row => ({
+      ...row,
+      line: Number(row.line), // or just +row.line
+      depth: Number(row.depth),
+      length: Number(row.length),
+      date: new Date(row.date + "T00:00" + row.timezone),
+      datetime: new Date(row.datetime)
+    }));
+
+    commits = d3.groups(data, d => d.commit).map(([commit, lines]) => {
+      let first = lines[0];
+      let {author, date, time, timezone, datetime} = first;
+      let ret = {
+        id: commit,
+        url: "https://github.com/vis-society/lab-7/commit/" + commit,
+        author, date, time, timezone, datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length
+      };
+
+      // Like ret.lines = lines
+      // but non-enumerable so it doesn’t show up in JSON.stringify
+      Object.defineProperty(ret, "lines", {
+        value: lines,
+        configurable: true,
+        writable: true,
+        enumerable: false,
+      });
+
+      return ret;
+    });
+    console.log(commits);
+  });
+</script>
 
 <h1>Meta</h1>
-<p>WIP</p>
+<section>
+  <dl class="stats">
+    <dt>Total <abbr title="Lines of code">LOC</abbr></dt>
+    <dd>{data.length}</dd>
+  </dl>
+  <dl class="stats">
+    <dt>Total commits</dt>
+    <dd>{commits.length}</dd>
+  </dl>
+  <dl class="stats">
+    <dt>Max lines</dt>
+    <dd>{Math.max(...commits.map(a => a.totalLines))}</dd>
+  </dl>
+</section>
+<h3>Commits by time of day</h3>
+<svg viewBox="0 0 {width} {height}">
+  <g class="gridlines" transform="translate({usableArea.left}, 0)" bind:this={yAxisGridlines} />
+  <g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
+  <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
+	<g class="dots">
+    {#each commits as commit, index }
+      <circle
+        cx={ xScale(commit.datetime) }
+        cy={ yScale(commit.hourFrac) }
+        r="5"
+        fill="steelblue"
+      />
+    {/each}
+    </g>    
+</svg>
+
+<style>
+	svg {
+		overflow: visible;
+	}
+
+  .gridlines {
+    stroke-opacity: .2;
+  }
+</style>
